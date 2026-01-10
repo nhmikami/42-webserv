3281c6ccf3c52bebce8e4e8aae302b23ec0c6d62
Caroline Yamashita Caleguer <cayamash@c2r8p3.42sp.org.br>
2026-01-10 18:48:57 -0300

    comandos curl para oficial.conf

--- data/oficial_curl.sh
+++ data/oficial_curl.sh
@@ -1,3 +1,5 @@
+# ================================================================================= #
+
 #Set variavel de ambiente
 URL="http://localhost:8084"
 
@@ -16,10 +18,24 @@ curl -i -b cookies.txt http://127.0.0.1:8084/deadly/deadly.go
 #location /forbidden
 curl -X GET "$URL/forbidden"
 
-# ================================================== #
+# ================================================================================= #
 
 #Set variavel de ambiente
 URL="http://localhost:8085"
 
 # location /
 curl -X GET "$URL"
+
+#location /cadet
+curl -X GET "$URL/cadet"
+curl -X POST "$URL/cadet" -F "photo=@photo.jpg" # fazer upload
+curl -X POST "$URL/cadet/cadet_create.py" -F "name=John Doe" -F "photo=@photo.jpg"
+
+#location /cadets_uploads
+curl -X GET "$URL/cadets_uploads"
+
+#location /cadets_list
+curl -X GET "$URL/cadets_list"
+curl -X DELETE "$URL/cgi/cadet_delete.py?name=John+Doe&file=person.jpg"
+
+# ================================================================================= #
